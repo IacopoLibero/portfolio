469f613303a76fd929c5ff6e0c5022a1075738f8
pc nebula <iacopoliberolavoro@gmail.com>
2025-05-28 17:27:41 +0200

    Add @vercel/analytics dependency and import in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import classNames from "classnames";
 
 import { Footer, Header, RouteGuard } from "@/components";
 import { baseURL, effects, style, font, home } from "@/app/resources";
+import { Analytics } from "@vercel/analytics/next"
 
 import { Background, Column, Flex, ThemeProvider, ToastProvider } from "@/once-ui/components";
 import { opacity, SpacingToken } from "@/once-ui/types";
